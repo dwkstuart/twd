531c3cdb2837e230a898bdafdd3ad3d64e87628d
dwkstuart <0207805s@student.gla.ac.uk>
2017-01-26 11:08:57 +0000

    not working properly

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
+
 #def index(request):
  #   return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
+def show_category(request, category_name_slug):
+	context_dict = {}
+	
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		
+		pages = Page.objects.filter(category=category)
+		
+		context_dict['category'] = category
+		
+	
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+		
+	return render(request, 'rango/category.html', context_dict)
 
-
+	
+	
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.object.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
 	
+   
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
