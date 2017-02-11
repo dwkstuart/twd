396940ebfb1ab68a7b006612bbe34485f34e1df3
unknown <0207805s@BO1028-6-07.ad.dcs.gla.ac.uk>
2017-02-11 17:37:59 +0000

    start fo chapter 9 need pillow

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
@@ -38,5 +39,16 @@ class Page(models.Model):
 	def __unicode__(self): 
 		return self.title
 		
-
+class UserProfile(models.Model):
+	
+	user = models.OneToOneField(User)
+	
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	
+	def __str__(self):
+		return self.user.username
+		
+	def __unicode__(self):
+		return self.user.username
 	
