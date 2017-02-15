1cbef621773a50b069005a18404083510a4f30e3
unknown <0207805s@BO1028-5-07.ad.dcs.gla.ac.uk>
2017-02-15 15:08:49 +0000

    chapter10 exercises

--- rango/views.py
+++ rango/views.py
@@ -52,17 +52,17 @@ def show_category(request, category_name_slug):
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    request.session.set_test_cookie()
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': page_list}
+	request.session.set_test_cookie()
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages': page_list}
 
-    response = render(request, 'rango/index.html', context_dict)
-    visitor_cookie_handler(request, response)
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return response
+    
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+	
+	response = render(request, 'rango/index.html', context_dict)
+	return response
 
 def about(request):
     if request.session.test_cookie_worked():
@@ -77,22 +77,30 @@ def about(request):
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/about.html', context=context_dict)
 
-def visitor_cookie_handler(request, response):
+def visitor_cookie_handler(request):
 
-    visits = int(request.COOKIES.get('visits', '1'))
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
 
     last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                         '%Y-%m-%d %H:%M:%S')
-    if (datetime.now() - last_visit_time).days >0:
-        visits = visits +1
-        response.set_cookie('last_visit', str(datetime.now()))
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] =str(datetime.now())
     else:
         visits = 1
 
-        response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
 
-    response.set_cookie('visits', visits)
+    request.session['visits'] = visits
+	
+#a helper method
+
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
 
 def add_page(request, category_name_slug):
     try:
